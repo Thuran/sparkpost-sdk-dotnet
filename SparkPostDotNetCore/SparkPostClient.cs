7f32dd04c56147eda9b7534f187ac8b53038eb7d
Thuran <thuran_eduardo@hotmail.com>
2017-05-18 17:33:26 -0300

    Initial

---
+++ SparkPostDotNetCore/SparkPostClient.cs
@@ -0,0 +1,35 @@
+﻿namespace SparkPostDotNet
+{
+    using System;
+    using System.Net.Http;
+    using System.Threading.Tasks;
+    using Transmissions;
+
+    public class SparkPostClient
+    {
+        
+
+        protected Uri SparkPoostUri => new Uri("https://api.sparkpost.com/api/v1/transmissions");
+
+        public async Task CreateTransmission(Transmission transmission)
+        {
+            const string apiKey = "Sua Api Key";
+            if (string.IsNullOrEmpty(apiKey))
+            {
+                throw new InvalidOperationException("Configuration variable SparkPost:ApiKey must be set.");
+            }
+
+            using (var httpClient = new HttpClient())
+            {
+                transmission.Headers.Clear();
+                transmission.Headers.Add("Content-Type", "application/json");
+                httpClient.DefaultRequestHeaders.Add("Authorization", apiKey);
+                var response = await httpClient.PostAsync(this.SparkPoostUri, transmission);
+                if(!response.IsSuccessStatusCode)
+                {
+                    throw new Exception(await response.Content.ReadAsStringAsync());
+                }
+            }
+        }
+    }
+}
